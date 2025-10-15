fdbbcce2d2bb2791087947617b64cf2a72e068f0
khushboo545567 <khushbooyadav6266763530@gmail.com>
2025-10-16 01:57:29 +0530

    reset count

--- reduxtoolkitfol/src/App.jsx
+++ reduxtoolkitfol/src/App.jsx
@@ -1,14 +1,24 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, reset } from "../features/counter/counterSlice";
 
 function App() {
   const count = useSelector((state) => state.counter.value);
-  const handleIncrementClick = () => {};
-  const handledecrementClick = () => {};
+  const dispatch = useDispatch();
+  const handleIncrementClick = () => {
+    dispatch(increment());
+  };
+  const handledecrementClick = () => {
+    dispatch(decrement());
+  };
+  const handleResetClick = () => {
+    dispatch(reset());
+  };
   return (
     <div>
       <button onClick={handleIncrementClick}>+</button>
-      <p>count:</p>
+      <p>count:{count}</p>
       <button onClick={handledecrementClick}>-</button>
+      <button onClick={handleResetClick}>reset</button>
     </div>
   );
 }
